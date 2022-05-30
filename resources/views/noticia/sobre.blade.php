ad8f1676b0c277815069411e21761c449967e98c
OduardoRuiz <carloseduardoruizs@gmail.com>
2022-05-30 00:17:04 -0300

    ajuste layouts - estruturação pagina Noticias

--- resources/views/noticia/sobre.blade.php
+++ resources/views/noticia/sobre.blade.php
@@ -1,21 +1,31 @@
 @include('layouts.header')
-<body>
+
+<body class="">
     @include('layouts.menu')
 
     @if (session()->has('sucesso'))
-        
-    <div>
-        {{session()->get('sucesso')}}
+        <div>
+            {{ session()->get('sucesso') }}
 
-    </div>
- @endif
-    <h1>Sobre</h1>
+        </div>
+    @endif
+    <h1>Sobre nós</h1>
 
-    <h1>pagina para o todos</h1>
     <main>
-        
-            <div>sobre nos</div>
-        
+
+        <div>
+            <span> Nossa missão é muito mais que ser simplesmente um negócio pet . Nosso objetivo maior, é encantar os
+                tutores e seus filhos de patas com uma experiência emocional positiva…Ajudar “pais e mães” de pets a
+                melhorarem a saúde e qualidade de vida de seus peludos.
+            </span>
+
+        </div>
+        <div class="imgSobre">
+            <img class="img-fluid" src="https://storage.googleapis.com/petmiau-imagens/front-end/petmiaulogotransparente1.png" alt="Logo Pet miau">
+
+        </div>
+
+
     </main>
     @include('layouts.footer')
 
